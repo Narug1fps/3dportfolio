5503e7720ed32541476ba3150cec628d16e47113
Narug1fps <144721920+Narug1fps@users.noreply.github.com>
2025-12-20 22:17:59 -0300

    Atualizar o page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     return () => window.removeEventListener('resize', checkMobile);
   }, []);
 
-  const pagesValue = isMobile ? 5.5 : 4.14;
+  const pagesValue = isMobile ? 4.7 : 4.14;
 
   return (
     <>
